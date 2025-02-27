c94274f264abf8653d2be38678916935c32fc342
John Doe <sherkeemduprey@gmail.com>
2025-02-26 20:07:16 -0500

    added new events.py

---
+++ src/events.py
@@ -0,0 +1,20 @@
+from src.person import Player
+class Events:
+    def __init__(self,person) -> None:
+        self.person = Player()
+        pass
+
+    '''
+    What should there be? In b-life there are random events based on age, life status, riches,
+    health conditions, relationshsips
+
+    For right now we could maybe add stuff that'll happen to babies, stuff that'll happen randomly,
+    all of which could affect our money, health, happiness 
+    '''
+
+    def randomEvent(self, person) -> None:
+        #TODO: Create this 
+
+        pass
+    
+    
