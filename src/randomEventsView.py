87be59dee6e87fe1ac14d519dbd00740159895dc
John Doe <sherkeemduprey@gmail.com>
2025-03-05 15:05:13 -0500

    the buttons all in correct places now. working on frame for random events

--- src/randomEventsView.py
+++ src/randomEventsView.py
@@ -3,17 +3,20 @@ from src.events import Events
 
 
 class RandomEventFrame(customtkinter.CTkFrame):
-    def __init__(self, master, person) -> None:
+    def __init__(self, master, person,windows) -> None:
         super().__init__(master)
+        self.windows = windows
         self.person = person
         
         self.randomEvents = Events(self.person)
         if self.person.age<18:
-            self.generateBabyEvent()
+            self.closeAllFrames()
+            print("hi")
         
         self.closeButton = customtkinter.CTkButton(self, text="Close", command=self.closeFrame)
         self.closeButton.grid(row=0, column=1, padx=10,
                           pady=(10, 0), sticky="w")
+        
 
     
     def generateBabyEvent(self) -> None:
@@ -21,9 +24,24 @@ class RandomEventFrame(customtkinter.CTkFrame):
         self.textBox = customtkinter.CTkLabel(self, text=randomText, fg_color='gray', text_color='black')
         self.textBox.grid(row=0, column=0, padx=10,
                              pady=(10, 0), sticky="w")
+        self.windows +=1
+        print("hi")
     
     def closeFrame(self) -> None:
+        self.windows -=1
         self.destroy()
+    
+    def closeAllFrames(self) -> None:
+        while self.windows> 0:
+            print(f"hi {self.windows}")
+            self.windows -=1
+            self.destroy()
+            
+        if self.winfo_exists():      
+            self.generateBabyEvent()
+    
+    def windowExists(self) -> None:
+        return self.windows != 0
         
 
     
--- src/window.py
+++ src/window.py
@@ -32,17 +32,17 @@ class App(customtkinter.CTk):
         
         self.ageLabel = customtkinter.CTkLabel(
             self, text=f"Age: {self.player.getAge()}", fg_color="yellow", text_color="black")
-        self.ageLabel.grid(row=0, column=0, padx=0, pady=0, sticky="w")
+        self.ageLabel.grid(row=0, column=0, padx=15, pady=0, sticky="sw")
 
         self.moneyLabel = customtkinter.CTkLabel(
             self, text=f"Money: ${self.player.getMoney()}", fg_color="yellow", text_color="black")
-        self.moneyLabel.grid(row=1, column=1, padx=0, pady=0, sticky="w")
+        self.moneyLabel.grid(row=1, column=0, padx=15, pady=0, sticky="nw")
 
         self.happinessLabel = customtkinter.CTkLabel(self, text=f"Happiness: {self.player.getHappiness()}",fg_color = "yellow", text_color = "black")
-        self.happinessLabel.grid(row=1, column=0, padx=0, pady=0, sticky="w")
+        self.happinessLabel.grid(row=0, column=1, padx=15, pady=0, sticky="se")
 
         self.healthLabel = customtkinter.CTkLabel(self, text=f"Health: {self.player.getHealth()}",fg_color="yellow", text_color="black")
-        self.healthLabel.grid(row =3 , column = 0, padx = 0, pady = 0, sticky = 'w')
+        self.healthLabel.grid(row =1 , column = 1, padx = 15, pady = 0, sticky = 'ne')
 
 
         self.ageUpButton()
@@ -50,14 +50,14 @@ class App(customtkinter.CTk):
    
     def ageUpButton(self):
         self.ageUpBtn = customtkinter.CTkButton(
-            self, text="Age up!", command=self.updateLabels, fg_color="yellow", text_color='black', hover_color='gray'
+            self, text="Age up!", command=self.updateLabels, fg_color="yellow", text_color='black', hover_color='gray', height=10
         )
-        self.ageUpBtn.grid(row=16, column=0, columnspan=2,
-                           padx=20, pady=20, sticky='ew')
+        self.ageUpBtn.grid(row=7, column=0, columnspan=2,
+                           padx=20, pady=20, sticky='s')
 
     def updateLabels(self):
         self.player.ageUp()
-        self.testThing = RandomEventFrame(self, self.player)
+        self.testThing = RandomEventFrame(self, self.player, 0 )
         self.testThing.grid(
             row=0, column=1, padx=10, pady=(10, 0))
         
@@ -65,14 +65,19 @@ class App(customtkinter.CTk):
         self.ageLabel.configure(text=f"Age: {self.player.getAge()}")
         self.happinessLabel.configure(text=f"Happiness: {self.player.getHappiness()}")
         self.healthLabel.configure(text=f"Health: {self.player.getHealth()}")
+        
+        
+        if self.testThing.windowExists():
+            print("ok, im seeing a window exists")
+            self.testThing.closeAllFrames()
 
     
     def createLife(self):
         self.player = Player(0.0, 100, 0)
         self.storyText = Stories()
         self.textLabel = Label(self)
-        self.textLabel.grid(row=1, column=0, rowspan = 2, columnspan=2,
-                             padx=20, pady=10, sticky='ew')
+        self.textLabel.grid(row=6, column=0, rowspan = 2, columnspan=2,
+                             padx=20, pady=20, sticky='sew')
         self.textLabel.addText(self.storyText.generateBirth(
             self.player.getPlaceOfBirth()))
 
@@ -83,15 +88,23 @@ class App(customtkinter.CTk):
 
 
 app = App()
-app.attributes("-fullscreen", True)  #make fullscreen for mac
+#app.attributes("-fullscreen", True)  #make fullscreen for mac
 app.bind("<Escape>", lambda e: app.attributes("-fullscreen", False))
 
 
-for i in range(16): 
-    app.grid_rowconfigure(i, weight=1)
+for i in range(8): 
+    app.grid_rowconfigure(i, weight=0, minsize=100)
 
 for j in range(2):  
     app.grid_columnconfigure(j, weight=1)
+
+app.grid_rowconfigure(0,weight=1)
+app.grid_rowconfigure(1, weight=1)
+app.grid_rowconfigure(2, weight=0)
+app.grid_rowconfigure(3, weight=1)
+#for age up button to actually go to the bottom of the window lol
+app.grid_rowconfigure(7, weight=1) 
+app.grid_rowconfigure(6,weight = 1) 
 
 app.mainloop()
 
